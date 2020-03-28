ec7c8d1cf27631f1c3d9bb8f0464661af7794a0c
Victor Amaral <victoramaral@usp.br>
2020-03-27 22:26:42 -0300

    primeira função: write()

--- write_on_screen.c
+++ write_on_screen.c
@@ -1,9 +1,14 @@
-#include <unistd.h>
-
-
+#include <unistd.h>  // unistd.h é uma biblioteca que fornece API wrapper para chamadas de sistema do SO, usando o padrão POSIX (IEEE).
+#include <stdio.h>
+#include <time.h>
 
 int main(){
-    char frase[50] = "This is a different Hello World.";
-    write(0, &frase, 50);
+    char frase[50] = "This is a different Hello World.\n";
+    clock_t inicio,fim;
+    inicio = clock();
+    write(0, &frase, 50); // essa função utiliza a syscall write. O primeiro argumento é o descritor do arquivo ( 0 para out, 1 para input, 2 para stderr).
+    fim = clock();
+    double tempo = (fim - inicio)*1000.0/CLOCKS_PER_SEC;
+    printf("Tempo gasto: %g mS\n", tempo);
     return 0;
 }
