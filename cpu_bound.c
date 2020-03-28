62ac55b5fdb453204c68a30fa2ff37c226c6cb9f
Victor Amaral <victoramaral@usp.br>
2020-03-28 14:49:16 -0300

    parte 2 feita

---
+++ cpu_bound.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define TAM 500  // DEFINE O TAMANHO DA MATRIX QUADRADA
+#define FAIXA 100  // DEFINE A FAIXA DE VALORES DOS ELEMENTOS
+/*
+CRIA UMA MATRIZ ALEATÓRIA DE TAM ELEMENTOS.
+*/
+double** create_rand_matrix(int tam){
+  double **x = (double**)calloc(tam, sizeof(double));
+  for(int i = 0; i < tam; i++)
+    x[i] = (double*)calloc(tam, sizeof(double));
+  for(int i = 0; i < tam; i++)
+    for(int j = 0; j < tam; j++)
+      x[i][j] = rand()%FAIXA;
+  return x;
+}
+
+
+/*
+  LIBERA A MATRIZ
+*/
+void free_matrix(double** matrix, int tam){
+  for(int i = 0; i < tam; i++)
+    free(matrix[i]);
+}
+
+
+/*
+  Multiplica duas matrizes quadradas, gerando uma terceira
+*/
+double** multiplica_matriz(double** matrix1, double** matrix2){
+  double** matrix3 = create_rand_matrix(TAM);
+  for(int i = 0; i < TAM; i++){
+    for(int j = 0; j < TAM; j++){
+      double soma = 0;
+      for(int k = 0; k < TAM; k++){
+        soma+= matrix1[i][k]*matrix2[k][j];
+        
+      }
+      matrix3[i][j] = soma;
+    }
+  }
+  return matrix3;
+}
+int main(){
+  srand(time(NULL));
+  double** matriz1 = create_rand_matrix(TAM);
+  double** matriz2 = create_rand_matrix(TAM);
+  double** resultado = multiplica_matriz(matriz1,matriz2);
+  free_matrix(matriz1, TAM);
+  free_matrix(matriz2, TAM);
+  free_matrix(resultado, TAM);
+  return 0;
+}
--- write_on_screen.c
+++ write_on_screen.c
@@ -1,14 +1,38 @@
 #include <unistd.h>  // unistd.h é uma biblioteca que fornece API wrapper para chamadas de sistema do SO, usando o padrão POSIX (IEEE).
+#include <fcntl.h>
 #include <stdio.h>
 #include <time.h>
 
+void limpa_string(char* string, int tam){
+    for(int i = 0; i < tam; i++)
+        string[i] = '\0';
+    return;
+}
 int main(){
-    char frase[50] = "This is a different Hello World.\n";
-    clock_t inicio,fim;
-    inicio = clock();
+    clock_t inicio,fim; // DECLARAÇÕES
+    int file_descriptor;
+    char frase[50];
+    limpa_string(frase, 50);
+    double tempo;
+    
+    inicio = clock();   // ABRIR ARQUIVO
+    file_descriptor = open("file.txt", O_RDONLY); // Syscall para abrir um arquivo e gerar um file_descriptor.
+    fim = clock();
+    tempo = (fim - inicio)*1000.0/CLOCKS_PER_SEC;
+    printf("Tempo para abrir o arquivo: %g mS\n", tempo);
+
+    inicio = clock(); // LER ARQUIVO
+    read(file_descriptor, frase, 50);
+    fim = clock();
+    tempo = (fim - inicio)*1000.0/CLOCKS_PER_SEC;
+    printf("Tempo para ler do arquivo: %g mS\n", tempo);
+
+    inicio = clock(); // ESCREVER NA TELA
     write(0, &frase, 50); // essa função utiliza a syscall write. O primeiro argumento é o descritor do arquivo ( 0 para out, 1 para input, 2 para stderr).
     fim = clock();
-    double tempo = (fim - inicio)*1000.0/CLOCKS_PER_SEC;
-    printf("Tempo gasto: %g mS\n", tempo);
+    tempo = (fim - inicio)*1000.0/CLOCKS_PER_SEC;
+    printf("Tempo gasto na escrita: %g mS\n", tempo);
+
+
     return 0;
 }
